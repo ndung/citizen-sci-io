112f3bc088d29385d49c775bf25837a66985dc13
ndung <ndoenks@gmail.com>
2025-08-21 13:13:00 +0700

    modified ProjectApiController

--- src/main/java/io/sci/citizen/api/ProjectApiController.java
+++ src/main/java/io/sci/citizen/api/ProjectApiController.java
@@ -1,19 +1,27 @@
 package io.sci.citizen.api;
 
-import io.sci.citizen.model.Project;
 import io.sci.citizen.service.ProjectService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
-public class ProjectApiController {
+public class ProjectApiController extends BaseApiController{
+
     private final ProjectService service;
+
     public ProjectApiController(ProjectService service) { this.service = service; }
 
     @GetMapping
-    public List<Project> all() {
-        return service.findAll();
+    public ResponseEntity<Response> get(@RequestHeader("Authorization") String token) {
+        try {
+            if (!authorize(token)) {
+                return FORBIDDEN;
+            }
+            return getHttpStatus(new Response(service.findAll(Long.parseLong(getUserId(token)))));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return getHttpStatus(new Response(e.getMessage()));
+        }
     }
 }
